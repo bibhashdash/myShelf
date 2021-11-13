b064551c4c0c81391739f11cf26b6969ee91c61d
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-11-13 21:37:07 +0000

    card component created

---
+++ cards_script.js
@@ -0,0 +1,34 @@
+const titleCard = document.querySelector(".title-card");
+const titlecardHeading = document.querySelector(".title-card-heading");
+const microbuttonArrow = document.querySelector(".microbutton-arrow");
+const microbuttonClose = document.querySelector(".microbutton-close");
+const modalLayer = document.querySelector(".modal-layer");
+const textCard = document.querySelector(".text-card");
+
+titleCard.addEventListener("click", function() {
+    // titleCard.style.transform = "translateX(700px)";
+    titleCard.style.width = "0px";
+    titlecardHeading.style.transform = "translateX(700px)";
+    microbuttonArrow.style.transform = "translateX(700px)";
+    titleCard.style.transition = "all 1.4s";
+
+    titlecardHeading.style.transition = "all 0.5s";
+    microbuttonArrow.style.transition = "all 0.5s";
+
+    modalLayer.style.transform = "translateX(1px)";
+    modalLayer.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    modalLayer.style.transition = "all 1.4s";
+});
+
+microbuttonClose.addEventListener("click", function() {
+    modalLayer.style.transform = "translateX(-900px)";
+    modalLayer.style.backgroundColor = "white";
+    modalLayer.style.transition = "all 1.5s";
+    titleCard.style.width = "60%";
+    titlecardHeading.style.transform = "translateX(-10px)";
+    microbuttonArrow.style.transform = "translateX(-10px)";
+    titleCard.style.transition = "all 1s";
+
+    titlecardHeading.style.transition = "all 2.6s";
+    microbuttonArrow.style.transition = "all 2.6s";
+});
