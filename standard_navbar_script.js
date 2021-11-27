07625d255bc23e71282515ce5dd9e00672dcd4ed
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-11-27 20:41:26 +0000

    fixed close button positioning for navbar

--- standard_navbar_script.js
+++ standard_navbar_script.js
@@ -4,14 +4,14 @@ const closebtn = document.querySelector(".closebtn");
 
 hamburger.addEventListener("click", function() {
     navlinks.classList.add("navlinks-open");
-    navlinks.style.transition = "all 0.7s";
+    navlinks.style.transition = "ease-in-out 0.4s";
     hamburger.style.opacity = "0";
     closebtn.classList.remove("hidden");
 });
 
 closebtn.addEventListener("click", function() {
     navlinks.classList.remove("navlinks-open");
-    navlinks.style.transition = "all 0.7s";
+    navlinks.style.transition = "ease-in-out 0.7s";
     hamburger.style.opacity = "1";
     closebtn.classList.add("hidden");
 });
